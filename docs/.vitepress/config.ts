dd3a048277f6db12a9db08ef261df34384ee1065
Shicosoft <shicoweb@gmail.com>
2024-05-15 15:04:40 +0300

    chore: Change Docs Title

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: 'Reverse Proxy',
+  title: 'TLSX Docs',
   description: 'A better developer environment.',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
