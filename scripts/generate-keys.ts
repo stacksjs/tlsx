d3c5b413facd6e65e4f92c08c5968f9ccff0e36d
Chris <chrisbreuer93@gmail.com>
2024-11-14 11:35:40 +0100

    chore: remove unused scripts

--- scripts/generate-keys.ts
+++
@@ -1,7 +0,0 @@
-// Generate a root key and certificate (self-signed)
-// await generateRootCA()
-
-// Generate a keypair and create an X.509v3 certificate for the domain
-// await generateAndSaveCertificates()
-
-// await addRootCAToSystemTrust()
