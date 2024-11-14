5f8b22d8b930f4edcbc0ddd458085610d789b4d9
Chris <chrisbreuer93@gmail.com>
2024-11-14 11:08:54 +0100

    chore: remove unused calls

--- build.ts
+++ build.ts
@@ -33,5 +33,3 @@ await $`cp ./dist/src/index.js ./dist/index.js`
 await $`rm -rf ./dist/src`
 await $`cp ./dist/bin/cli.js ./dist/cli.js`
 await $`rm -rf ./dist/bin`
-await $`cp ./bin/cli.d.ts ./dist/cli.d.ts` // while bun-plugin-dts-auto doesn't support bin files well
-await $`rm ./bin/cli.d.ts`
